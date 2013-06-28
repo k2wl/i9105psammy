c2bee6fcad7323bbfa40914b396a660eede88731
k2wl <k2wl@yahoo.com>
2013-06-28 11:25:10 +0530

    new governers

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -2,7 +2,7 @@
 #include <linux/broadcom/vc_debug_sym.h>
 
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "Android-ThinkPad-T400" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "388e4014fb977d31b6363138bb352315ca5cce24 (tainted)" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "c55b2687f0418d1715a0eff79a21c5ed7436664a (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
