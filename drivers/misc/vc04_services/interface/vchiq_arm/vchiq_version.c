b4af46e4f951630fa58618bad59d0e520a78bdbe
k2wl <k2wl@yahoo.com>
2013-06-29 13:16:17 +0530

    new additions

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -2,7 +2,7 @@
 #include <linux/broadcom/vc_debug_sym.h>
 
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "Android-ThinkPad-T400" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "864b6e7915d14161de92530e6edee3c3a46e3393 (tainted)" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "b6f231cb36b1a4be97f81b17072e52aae9fa3ce9 (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 0 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Sat Jun 29 11:51:19 IST 2013"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 29 12:22:49 IST 2013"
 #define LINUX_COMPILE_BY "kar"
 #define LINUX_COMPILE_HOST "Android-ThinkPad-T400"
 #define LINUX_COMPILER "gcc version 4.7.4 20130603 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.06) "
